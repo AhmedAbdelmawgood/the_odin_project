a82be0de077cb881a4c53aafbfa3dc0d4c411631
ahmed samir <mody.ahmed1350@yahoo.com>
2016-02-24 20:44:50 +0200

    initial commit of  blogger project

---
+++ rails/blogger/app/controllers/articles_controller.rb
@@ -0,0 +1,36 @@
+class ArticlesController < ApplicationController
+	include ArticlesHelper
+	def index 
+		@articles = Article.all	
+	end
+	def show
+		@article = Article.find(params[:id])
+		@comment = Comment.new
+		@comment.article_id = @article.id 
+	end
+
+	def new 
+		@article = Article.new
+	end
+	def edit
+		@article= Article.find(params[:id])
+	end
+	def create 
+		@article = Article.new(article_params)
+		@article.save
+		flash.notice = "Article #{@article.title} is created"
+		redirect_to(article_path(@article))
+	end
+	def destroy
+		@article = Article.find(params[:id])
+		flash.notice = "Article #{@article.title} is destroied"
+		@article.destroy
+		redirect_to articles_path
+	end
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+		flash.notice = "Article #{@article.title} was updated"
+		redirect_to(article_path(@article))
+	end
+end
---
+++ rails/blogger/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root to: 'articles#index'
+  resources :articles do 
+  	resources :comments
+  	end
+end
